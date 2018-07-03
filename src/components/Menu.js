1cb0e2d2cbaae7d48d1eb88f904ded9eeb344db6
Bus42 <gbrewton1980@gmail.com>
2018-07-03 13:03:08 -0500

    Adds modal to player count in menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
+import { Modal, Glyphicon, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+    this.open = this.open.bind(this);
+    this.close = this.close.bind(this);
+  }
+
+  close() {
+    this.setState({ showModal: false });
+  }
+
+  open() {
+    this.setState({ showModal: true });
+  }
+
   render() {
     return (
-      <ul className="list-unstyled">
-        <li>Player Count</li>
-        <li>Background</li>
-        <li>Font</li>
-        <li>Rules</li>
-      </ul>
+      <div className="well ListGroup-wrapper">
+        <ListGroup>
+          <ListGroupItem>
+            <p onClick={this.open}>Player Count</p>
+            <Modal show={this.state.showModal} onHide={this.close}>
+              <Modal.Header>
+                <Modal.Title>Player Count</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <p>How many players will be in this match?</p>
+              </Modal.Body>
+              <Modal.Footer>
+                <Glyphicon glyph="remove-circle" onClick={this.close} />
+              </Modal.Footer>
+            </Modal>
+          </ListGroupItem>
+          <ListGroupItem>
+            <p>Background</p>
+          </ListGroupItem>
+          <ListGroupItem>
+            <p>Font</p>
+          </ListGroupItem>
+          <ListGroupItem>
+            <p>Rules</p>
+          </ListGroupItem>
+        </ListGroup>
+      </div>
     );
   }
 }
