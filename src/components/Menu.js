0b22b1224e84d40043d5b269f1f845975f29071c
Bus42 <gbrewton1980@gmail.com>
2018-07-03 13:24:33 -0500

    Fixes layout issues with menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { Modal, Glyphicon, ListGroup, ListGroupItem } from 'react-bootstrap';
+import {
+  Modal,
+  Glyphicon,
+  ListGroup,
+  ListGroupItem,
+  Well
+} from 'react-bootstrap';
 
 class Menu extends Component {
   constructor(props) {
@@ -21,32 +27,34 @@ class Menu extends Component {
 
   render() {
     return (
-      <div className="well ListGroup-wrapper">
-        <ListGroup>
-          <ListGroupItem>
-            <p onClick={this.open}>Player Count</p>
-            <Modal show={this.state.showModal} onHide={this.close}>
-              <Modal.Header>
-                <Modal.Title>Player Count</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <p>How many players will be in this match?</p>
-              </Modal.Body>
-              <Modal.Footer>
-                <Glyphicon glyph="remove-circle" onClick={this.close} />
-              </Modal.Footer>
-            </Modal>
-          </ListGroupItem>
-          <ListGroupItem>
-            <p>Background</p>
-          </ListGroupItem>
-          <ListGroupItem>
-            <p>Font</p>
-          </ListGroupItem>
-          <ListGroupItem>
-            <p>Rules</p>
-          </ListGroupItem>
-        </ListGroup>
+      <div className="menuDiv-wrapper">
+        <Well bsSize="small">
+          <ListGroup>
+            <ListGroupItem>
+              <p onClick={this.open}>Player Count</p>
+              <Modal show={this.state.showModal} onHide={this.close}>
+                <Modal.Header>
+                  <Modal.Title>Player Count</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <p>How many players will be in this match?</p>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Glyphicon glyph="remove-circle" onClick={this.close} />
+                </Modal.Footer>
+              </Modal>
+            </ListGroupItem>
+            <ListGroupItem>
+              <p>Background</p>
+            </ListGroupItem>
+            <ListGroupItem>
+              <p>Font</p>
+            </ListGroupItem>
+            <ListGroupItem>
+              <p>Rules</p>
+            </ListGroupItem>
+          </ListGroup>
+        </Well>
       </div>
     );
   }
